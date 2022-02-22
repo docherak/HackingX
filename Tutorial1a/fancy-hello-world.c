7585b44de25d02e63f6f5c706959d729ceac9ae8
docherak <docherak@protonmail.ch>
2022-02-22 16:32:33 +0100

    Fixed fancy-hello-world.c

--- Tutorial1a/fancy-hello-world.c
+++ Tutorial1a/fancy-hello-world.c
@@ -3,20 +3,7 @@
 #include "fancy-hello-world.h"
 
 int main(void) {
-	/* VERSION1
-	char output[] = "Hello World, hello ";
-
-	char name[64];
-	printf("Enter the name:\n");
-	fgets(name, 64, stdin);
-
-	hello_string(name, output);			
-
-	return 0;
-	*/
-
-	// VERSION2
-	char output[100], name[100];		// init variables (parameters)
+	char name[101], output[201];		// init variables (parameters)
 
 	hello_string(name, output);		// call the function
 
@@ -24,15 +11,9 @@ int main(void) {
 }
 
 void hello_string(char* name, char* output) {	
-	/* VERSION1
-	strcat(output, name);
-	printf("%s", output);		
-	*/
-
-	// VERSION2
 	printf("Enter the name:\n");		// pre-input question	
 	
-	strcpy(name, fgets(name, 100, stdin));	// copy inputted string to "name"
+	strcpy(name, fgets(name, 101, stdin));	// copy inputted string to "name"
 	strcpy(output, "Hello World, hello ");	// copy the string to output
 	
 	strcat(output, name);			// append name to output
